a0c1e91d95dbbe83ff38d0b310f9c377a6e72476
egochao <johnoldmanworkspace@gmail.com>
2021-06-12 08:00:05 +0000

    add encoder

--- modules/encoder.py
+++ modules/encoder.py
@@ -2,10 +2,18 @@ import torch
 from torch import nn
 from .attention import CustomMultiHeadAttention
 from .blocks import PositionwiseFeedForward, CustomLayerNorm
+from .position_layers import PositionEncoding
 
 
 class CustomEncoderLayer(nn.Module):
     def __init__(self, dim, n_head, ffn_hidden=None, dropout=0.0):
+        """
+        Encoder Block
+        :param dim: Embedding dimension
+        :param n_head: Number of head in multi head attention
+        :param ffn_hidden: Number of hidden nodes of feed forward layer
+        :param dropout: Dropout rate in the block
+        """
         super(CustomEncoderLayer, self).__init__()
         self.multiheadatt = CustomMultiHeadAttention(dim, n_head)
         self.norm1 = CustomLayerNorm(dim)
@@ -36,4 +44,33 @@ class CustomEncoderLayer(nn.Module):
         return x
 
 
+class CustomEncoder(nn.Module):
+    def __init__(self, vocab_size, max_len, d_embed, ffn_hidden, n_head, n_layers, dropout):
+        """
+        Encoder (n x encode layer)
+        :param vocab_size: Input vocab size for embedding
+        :param max_len: Maximum length of position embedding
+        :param d_embed: Embedding dimension
+        :param ffn_hidden: Number of hidden nodes of feed forward layer
+        :param n_head: Number of head in multi head attention
+        :param n_layers: Number of repeated encoder layers
+        :param dropout: Dropout rate of encoder
+        """
+        super(CustomEncoder, self).__init__()
+        self.embed = nn.Embedding(vocab_size, d_embed, padding_idx=1)
+        self.position = PositionEncoding(d_embed, dropout=dropout, max_len=max_len)
 
+        self.layers = nn.ModuleList([CustomEncoderLayer(d_embed,
+                                                        n_head,
+                                                        ffn_hidden,
+                                                        dropout)
+                                     for _ in range(n_layers)])
+
+    def forward(self, x, mask):
+        x = self.embed(x)
+        x = self.position(x)
+
+        for layer in self.layers:
+            x = layer(x, mask)
+
+        return x
--- tests/tests.py
+++ tests/tests.py
@@ -3,13 +3,26 @@ import torch
 
 sys.path.append("..")
 
-from modules.encoder import CustomEncoderLayer
+from modules.encoder import CustomEncoderLayer, CustomEncoder
+
+vocab_size = 1000
+max_len = 512
+d_embed = 32
+ffn_hidden = 32
+n_head = 4
+n_layers = 4
+dropout = 0.1
 
 
 def test_encoder():
     enlayer = CustomEncoderLayer(50, 5)
     x = torch.zeros((3, 100, 50), requires_grad=True)
     out = enlayer(x)
+    print(out.shape)
+
+    x = torch.randint(0, vocab_size, (300, 100))
+    encode = CustomEncoder(vocab_size, max_len, d_embed, ffn_hidden, n_head, n_layers, dropout)
+    out = encode(x, mask=None)
     print(out.shape)
 
 
